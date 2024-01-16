e8c0c1dfce9805b4d02f36625cc4f81926b6af23
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-01-16 21:56:09 +0500

    Added Quick Search Options

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ const App = () => {
       console.log(searchInput.current.value);
   });
 
+  // function for quick search option
+  const handleSelection = (selection) =>{
+      searchInput.current.value = selection;
+  }
   return (
     <>
         <div className="container">
@@ -23,6 +27,13 @@ const App = () => {
                   ref={searchInput}
               />
             </Form>
+          </div>
+          <div className="filters">
+            <div onClick={() => handleSelection('nature')}>Nature</div>
+            <div onClick={() => handleSelection('birds')}>Birds</div>
+            <div onClick={() => handleSelection('space')}>Space</div>
+            <div onClick={() => handleSelection('shoes')}>Shoes</div>
+            <div onClick={() => handleSelection('cats')}>Cats</div>
           </div>
         </div>
     </>
